bcbfa0b9a73dd467b49c992428f66f710b0a33f9
Matthew Li <02matthewli@gmail.com>
2022-05-17 21:01:39 -0400

    first tutorial

---
+++ Tutorials/Week 2 Tutorial/tutorial1.r
@@ -0,0 +1,19 @@
+# making the vectors
+Before2000 = c(13,1,1,1,1,1)
+After2000 = c(1,2,1,2,1,3,1)
+
+maxB2K = max(Before2000)
+
+beforeMean = mean(Before2000)
+afterMean = mean(After2000)
+
+differenceOfMeans = (beforeMean - afterMean)
+print(differenceOfMeans)    # can just write variable name (but I want better readability)
+
+# the band was more popular after 2000 as the average chart position
+# was higher (1.6) than before 2000 (3)
+
+# Matthew Li
+# Ciera Shannon
+# Sarita Samsoodeen
+# Ahmed Elsaeed
