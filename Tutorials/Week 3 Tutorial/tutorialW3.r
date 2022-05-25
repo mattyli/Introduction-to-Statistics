f95cbe0280d2250b2c5410e505ec6904b75443e3
Matthew Li <02matthewli@gmail.com>
2022-05-24 21:07:05 -0400

    update, added tutorial for week 3

--- Tutorials/Week 3 Tutorial/tutorialW3.r
+++ Tutorials/Week 3 Tutorial/tutorialW3.r
@@ -1,12 +1,12 @@
 # week 3 tutorial by ML
 setwd("C:/Users/02mat/OneDrive/Desktop/Intro-To-Stats/Introduction-to-Statistics/Tutorials/Week 3 Tutorial") #nolint
-chickData <- read.csv("sample_data.csv", colClasses=c("character", "character", "numeric", "numeric"))      # path to/ data file name
+chickData <- read.csv("chickweight_data.csv", colClasses=c("factor", "factor", "numeric", "numeric"))      # path to/ data file name
 
 # partitioning chick data in subsets based on diet
-controlSub <- subset(chickData, Diet=="1")
-d2Sub <- subset(chickData, Diet=="2")
-d3Sub <- subset(chickData, Diet=="3")
-d4Sub <- subset(chickData, Diet=="4")
+controlSub <- subset(chickData, Diet == "1")
+d2Sub <- subset(chickData, Diet == "2")
+d3Sub <- subset(chickData, Diet == "3")
+d4Sub <- subset(chickData, Diet == "4")
 
 # determining the mean weight of the chicks based on each diet
 # not calculating the average weight of each individual chick
@@ -17,22 +17,31 @@ d2WeightMean <- mean(d2Sub$Weight)
 d3WeightMean <- mean(d3Sub$Weight)
 d4WeightMean <- mean(d4Sub$Weight)
 
-# the scatterplot for diet 3
-# y axis should be average weight over set time
-# x axis should be time
+# printing the average weights
+print(controlWeightMean)
+print(d2WeightMean)
+print(d3WeightMean)
+print(d4WeightMean)
 
+# the scatterplot for diet 3, put into variables for cleanliness
 xAxis <- d3Sub$Time         # another dataframe containing all x values for scatter
 yAxis <- d3Sub$Weight
 
-plt = plot(xAxis, yAxis, pch=19, xlab = "Time (days)", ylab = "Weight (grams)", main = "Weight (grams) over Time (days) for chicks fed on diet 3",
-    col = c("red", "blue", "green", "black", "purple")
-)
+plt = plot(xAxis, yAxis, pch=19, xlab = "Time (days)", ylab = "Weight (grams)", main = "Weight (grams) over Time (days) for chicks fed on diet 3")
 
-# chick 1, 2, 3, 4, 5, = red, blue, green, black, purple
+# difference switching from diet A to diet B (function so we don't need to write the code again)
+# avgA/B correspond to the average chick weight of the corresponding diet
+calcPercentDifference <- function(avgA, avgB){
+    percentDiff <- 100 * (avgB - avgA) / avgA
+    return(percentDiff)
+}
 
-#x4 <- filter(d3Sub$Time, Chick == "4")
-#y4 <- filter(d3Sub$Weight, Chick == "4")
+# variables to hold the percent difference in weight
+oneToTwo <- calcPercentDifference(controlWeightMean, d2WeightMean)
+twoToThree <- calcPercentDifference(d2WeightMean, d3WeightMean)
+threeToFour <- calcPercentDifference(d3WeightMean, d4WeightMean)
 
-x4 <- d3Sub$Time[d3Sub$Chick == "4"]
-y4 <- d3Sub$Weight[d3Sub$Chick == "4"]
-pltC4 = plot(x4, y4, pch = 19, xlab = "Time (Days)", ylab = "Weight of Chick 4 (grams)", main = "Test Plot")
+# outputting the variables
+print(oneToTwo)
+print(twoToThree)
+print(threeToFour)
